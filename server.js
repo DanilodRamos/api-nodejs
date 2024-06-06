a19af46fd5be24a27476495e747178063648fcd3
Danilo Borges Ramos <116719740+DanilodRamos@users.noreply.github.com>
2024-06-06 11:46:45 -0300

    finalizando o backend

--- server.js
+++ server.js
@@ -1,20 +1,71 @@
-import express from 'express'//importanto biblioteca
+import express from 'express'                       //importanto biblioteca
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()                   //variavel e importacao biblioteca prisma
 
 const app = express()
-app.use(express.json())//avisando que vai chegar no express json
+app.use(express.json())                     //avisando que vai chegar no express json
+//criando usuarios post
+app.post('/usuarios', async (req, res) => {
 
-const users = [] //criando variavel users
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
 
-app.post('/usuarios', (req , res)=> {
-
-    users.push(req.body)// rotas tipo polt salvando usuarios dentro da variavel
+    })// mandando dentro do banco de dados essas requisiçoes
 
     res.status(201).json(req.body)//criando requisição req
 
-})//criando usuarios metodo post
+})
 
-app.get('/usuarios', (req, res) => {
-    res.status(200).json(users)//rotas respondendo com estatus enviando resposta com json listagem
-})//criando rota tipo get listar
+app.get('/usuarios', async (req, res) => {
+
+    let users = []
+    if (req.query) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
+            }
+        })
+
+    } else {
+        users = await prisma.user.findMany()
+    }
+
+
+    res.status(200).json(users)
+})
+//atualizando usuaris  put update
+app.put('/usuarios/:id', async (req, res) => {
+    await prisma.user.update({                      //prisma atualiza o usuario
+        where: {
+            id: req.params.id,
+        },                                 //quem vou atualizar
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        },                                        //informacoes novas
+
+    })                                               // mandando dentro do banco de dados essas requisiçoes
+
+    res.status(201).json(req.body)              //criando requisição req
+})                                              //criando usuarios metodo post
+
+//criando rota tipo deletar
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id //deleta pelo id
+        }
+    })
+    res.status(200).json({ message: "Usuario deletado com sucesso!" })//deletando usuario pelo id
+
+})
 
 app.listen(3000)//porta servidor que vai rodar
